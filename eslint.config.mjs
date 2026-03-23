a6da28e1af0dbaf686daad2a50965198d5ad9857
bbalabuya <tel030128@naver.com>
2026-03-23 16:33:09 +0900

    eslint 파일 추가

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import js from "@eslint/js";
+import globals from "globals";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    ignores: ["node_modules/", "dist/", "build/", "*.min.js"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "no-var": "error",
+      semi: "error",
+      eqeqeq: "warn",
+      "no-undef": "error", // 정의되지 않은 변수 참조시 에러
+      "prefer-const": "error", // 변수에 값을 재할당 하지 않으면 자동 const 변환
+    },
+  },
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.md"],
+    plugins: { markdown },
+    language: "markdown/commonmark",
+    extends: ["markdown/recommended"],
+  },
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"],
+  },
+]);
